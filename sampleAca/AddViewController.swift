4fb00875b46029c3f12704c5062a1448891f3223
uminonar <nupicker@gamil.com>
2016-05-24 19:51:05 +0800

    datePickerを２つに分けて設置　

--- sampleAca/AddViewController.swift
+++ sampleAca/AddViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class AddViewController: UIViewController {
     
+    @IBOutlet var addView: UIView!
+    
     // View動的追加用変数
+    var whenDatePicker = UIDatePicker()
+
+    var whenTimePicker = UIDatePicker()
+    
+    var bView = UIView(frame: CGRectMake(0, 0, 100, 100))
     var aView = UIView(frame: CGRectMake(0, 0, 100, 100))
     let myBoundSize: CGSize = UIScreen.mainScreen().bounds.size
     //var openFlag = false
@@ -32,49 +39,71 @@ class AddViewController: UIViewController {
         super.viewDidLoad()
         
         //色が変わらない！！！なぜ？
-
         let plus = FAKFontAwesome.plusIconWithSize(15)
         let plusImage = plus.imageWithSize(CGSizeMake(15, 15))
         addPlus.image = plusImage
-        
         
         let audio = FAKFontAwesome.fileAudioOIconWithSize(25)
         let audioImg = audio.imageWithSize(CGSizeMake(25, 25))
         addAudio.image = audioImg
         
-        
-        
         let plus2 = FAKFontAwesome.plusIconWithSize(25)
         let plus2Img = plus2.imageWithSize(CGSizeMake(25, 25))
         addPlusImg.image = plus2Img
         
-
         let addImage = FAKFontAwesome.fileImageOIconWithSize(50)
         let addI = addImage.imageWithSize(CGSizeMake(50, 50))
         addImg.image = addI
         
-        
         let plus3 = FAKFontAwesome.plusIconWithSize(25)
         let plus3Img = plus3.imageWithSize(CGSizeMake(25, 25))
         addPlusMovie.image = plus2Img
-    
         
         let addMo = FAKFontAwesome.fileMovieOIconWithSize(50)
         let addM = addMo.imageWithSize(CGSizeMake(50, 50))
         addMovie.image = addM
-
-        
     
     }
+    
     
     override func viewWillAppear(animated: Bool) {
      
         addName.text = add["name"]
         addSelfee.image = UIImage(named: "portrait1.JPG")
         
-        self.aView = UIView(frame: CGRectMake(0,myBoundSize.height,myBoundSize.width,250))
-        self.aView.backgroundColor = UIColor.grayColor()
+        self.bView = UIView(frame: CGRectMake(0,myBoundSize.height,myBoundSize.width,myBoundSize.height))
+        
+        self.bView.backgroundColor = UIColor.lightGrayColor()
+        
+        self.aView = UIView(frame: CGRectMake(0,myBoundSize.height,myBoundSize.width,270))
+        
+        self.aView.backgroundColor = UIColor.whiteColor()
+        
+     //        UIColor color = [UIColor .lightGrayColor()]
+     //        UIColor aColor = [color colorWithAlphaComponent:0.5];
+        
+        
+        whenDatePicker.maximumDate = NSDate()
+        
+        whenDatePicker.frame = CGRectMake(0,0,myBoundSize.width-100,270)
+//        whenDatePicker.datePickerMode = 
+        
+        
+        
+        whenTimePicker.frame = CGRectMake(myBoundSize.width-100,0,100,270)
+        
+        
+        let df = NSDateFormatter()
+    
+        df.dateFormat = "yyyy年MM月dd日"
+        
+        
+        
+        
         self.view.addSubview(aView)
+        aView.addSubview(whenDatePicker)
+        aView.addSubview(whenTimePicker)
+    
         
     }
     
@@ -98,16 +127,23 @@ class AddViewController: UIViewController {
     @IBAction func addWhenField(sender: UITextField) {
         print("addwhen")
         
-       
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            
+            
+            self.bView.frame = CGRectMake(0,0,self.myBoundSize.width,self.myBoundSize.height)
+            
+        })
+
         
         
         UIView.animateWithDuration(0.3, animations: { () -> Void in
             
+           
+            self.aView.frame = CGRectMake(0,self.myBoundSize.height-270,self.myBoundSize.width,270)
             
-            self.aView.frame = CGRectMake(0,self.myBoundSize.height-250,self.myBoundSize.width,250)
             
             
-            
+        
         })
     }
     
